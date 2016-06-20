5ee72b2c34313b7b664957919d1b81520cc9cff7
Marco Giglio <marcogiglio@gmail.com>
2016-06-20 23:00:13 +0200

    Using push

--- trustedaddress.sol
+++ trustedaddress.sol
@@ -11,34 +11,33 @@ contract TrustedAddress {
     function voteYes(address voteFor) {
         var voter   = msg.sender;
         var myVotes = votes[voter];
-        
+
         if (myVotes.length == 0) {
-            voters[voters.length++] = voter;
+            voters.push(voter);
         }
-        
+
         if (!votesMap[voter][voteFor]) {
-            myVotes[myVotes.length++] = voteFor;
+            myVotes.push(voteFor);
         }
-        
+
         votesMap[voter][voteFor] = true;
     }
-    
+
     function totalVoters() constant returns (uint) {
         return voters.length;
     }
-    
+
     function votersOfIndex(uint index) constant returns (address) {
         return voters[index];
     }
-    
+
     function totalVotesOf(address voter) constant returns (uint) {
         return votes[voter].length;
     }
-    
+
     function votesOf(address voter, uint index) constant returns (address, bool) {
         address voted = votes[voter][index];
         return (voted, votesMap[voter][voted]);
     }
 
 }
-
